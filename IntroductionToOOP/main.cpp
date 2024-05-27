fbea9048bb4cfd5d38ce93d56f95570c0e94a71c
Viktor <Viktor@VOKTOR-PC1>
2024-05-27 21:38:00 +0300

    Copy assignment DONE

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,74 +1,159 @@
-﻿#include <iostream>
-#include <math.h> //Математическая библиотека
-#include <iomanip>
-
+﻿#include<iostream>
 using namespace std;
 
-class Point //Создаем класс Point
+#define delimeter "\n"
+
+class Point
 {
-	//Создаем поля Класса
 	double x;
 	double y;
-
 public:
-	//Создаем конструкторы и методы класса
-	Point()//Конструктор по умолчанию
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+	//		Constructor:
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
 	{
 		this->x = 0;
 		this->y = 0;
-	};
-	Point(double x, double y)//Конструктор с параметрами
+		cout << "1ArgConstructor:\t " << this << endl;
+	}
+	Point(double x, double y)
 	{
 		this->x = x;
 		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
 	}
-	//Геттеры и Сеттеры для класса
-	double GetX()const
+	Point(const Point& other)
 	{
-		return this->x;
+		this->x = other.x;
+		this->y = other.y;
+		cout << " CopyConstructer:\t" << this << endl;
 	}
-	double GetY()const
+	~Point()
 	{
-		return this->y;
+		cout << "Destructor:\t" << this << endl;
 	}
-	void SetX(double x)
+	//		Operators
+	Point& operator=(const Point& other)
 	{
-		this->x = x;
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
 	}
-	void SetY(double y)
+	//		Methods:
+	double distance(const Point& other)const
 	{
-		this->y = y;
+
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+
+		return distance;
 	}
-	//Метод класса для подсчета расстояния между точками
-	double Distance(Point& object)
+	void print()const
 	{
-		return sqrt(pow((this->x - object.x), 2) + pow((this->y - object.y), 2));
+		cout << this << ":X = " << x << "\tY = " << y << endl;
 	}
 };
-
-//Функция для подсчета расстояния между точками
-double Distance(Point& object1, Point& object2)
+double distance(const Point& A, const Point& B)
 {
-	return sqrt(pow((object1.GetX() - object2.GetX()), 2) + pow((object1.GetY() - object2.GetY()), 2));
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	return sqrt(x_distance * x_distance + y_distance * y_distance);
 }
-
-int main()
+//#define STRUCT_POINT
+//#define DISTANCE_CHACK
+//#define CONSTRUCTORS_CHACK
+#define ASSIGNMENT_CHACK
+void main()
 {
-	setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
+	cout << "Hello,OOP" << endl;
+	Point A;
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+	cout << sizeof(A) << endl;
+#endif //STRUCT_POINT
+#ifdef DISTANCE_CHACK
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+	cout << delimeter << endl;
 
-	Point A;//Создаем объект А класса Point
-	A.SetX(2);//Заполняем поле Х
-	A.SetY(2);//Заполняем поле Y
+	/*Point D;
+	D.set_x(16.16);
+	D.set_y(9.25);
+	Point C;
+	C.set_x(4.25);
+	C.set_y(25.16);*/
 
-	Point B(3, 3);//Создаем объект B класса Point (заполнение полей через конструктор)
+	//distance(B.get_x(), B.get_y());
+	cout << "Расстояние от точки 'A' до точки 'B':" << A.distance(B) << endl;
+	cout << delimeter << endl;
+	cout << "Расстояние от точки 'B' до точки 'A':" << B.distance(A) << endl;
+	cout << delimeter << endl;
+	cout << "Расстояние между точками 'A' и 'B': " << distance(A, B) << endl;
+	cout << delimeter << endl;
+	cout << "Расстояние между точками 'B' и 'A': " << distance(A, B) << endl;
+	cout << delimeter << endl;
+#endif // DISTANCE_CHACK
 
-	//cout.precision(3);
-	cout << A.Distance(B) << endl;//Выводим резудьтат вычисления через метод класса
-	cout << Distance(A, B) << endl;//Выводим результат вычисления через функцию
+#ifdef CONSTRUCTORS_CHACK
+	Point A;	//DefaultConstructor
+	A.print();
 
-	system("pause");
+	Point B = 5;
+	B.print();
+	Point C(2, 3);
+	C.print();
+	Point D = C;
+	D.print();
+#endif // CONSTRUCTORS_CHACK
 
+#ifdef ASSIGNMENT_CHACK
+	//Point A(2, 3);
+	//Point B;	//
+	//B = A;
+	//B.print();
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << "\t" << b << "\t" << c << endl;
+	Point A, B, C;
+	cout << delimeter << endl;
+	A = B = C = Point(2, 3);
+	cout << delimeter << endl;
+	A.print();
+	B.print();
+	C.print();
+#endif //  define ASSIGNMENT_CHACK
 
-
-	return 0;
 }
