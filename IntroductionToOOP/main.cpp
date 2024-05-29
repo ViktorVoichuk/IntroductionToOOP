b6231693dd81911cab345359ffd081a29472dd0d
Viktor <Viktor@VOKTOR-PC1>
2024-05-29 21:41:44 +0300

    Copy assignment DONE

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -71,6 +71,19 @@ public:
 		cout << "CopyAssignment:\t\t" << this << endl;
 		return *this;
 	}
+	Point& operator++()//Префиксный инкремент
+	{
+		x++;
+		y++;
+		return *this;
+	}
+	Point operator++(int)//Постфиксный инкремент
+	{
+		Point old = *this;
+		x++;
+		y++;
+		return old;
+	}
 
 	//				Methods:
 	double distance(const Point& other)const
@@ -91,6 +104,7 @@ public:
 	{
 		cout << this << ":X = " << x << "\tY = " << y << endl;
 	}
+
 };
 
 double distance(const Point& A, const Point& B)
@@ -101,10 +115,57 @@ double distance(const Point& A, const Point& B)
 	//Функция sqrt() возвращает квадратный корень принятого числа
 }
 
+Point operator+(const Point& left, const Point& right)//Через функции.
+{
+	Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;
+}
+Point operator-(const Point& left, const Point& right)//Через конструктор
+{
+	Point result
+	(
+		left.get_x() - right.get_x(),
+		left.get_y() - right.get_y()
+	);
+	return result;
+}
+Point operator*(const Point& left, const Point& right)
+{
+	return Point
+	(
+		left.get_x() * right.get_x(),
+		left.get_y() * right.get_y()
+	);
+}
+Point operator/(const Point& left, const Point& right)
+{
+	return Point
+	(
+		left.get_x() / right.get_x(),
+		left.get_y() / right.get_y()
+	);
+}
+bool operator==(const Point& left, const Point& right)
+{
+	return left.get_x() == right.get_x() && left.get_y() == right.get_y();
+	/*if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}*/
+}
+
 #define STRUCT_POINT
 #define DISTANCE_CHECK
 #define CONSTRUCTORS_CHECK
 #define ASSIGNMENT_CHECK
+#define ARITHMETICAL_OPERATORS_CHECK
+
 
 void main()
 {
@@ -188,8 +249,34 @@ void main()
 	C.print();
 #endif // ASSIGNMENT_CHECK
 
-	
+#ifndef ARITHMETICAL_OPERATORS_CHECK
+	int a = 2;
+	int b = 3;
+	int c = a + b;
 
+	Point A(2, 3);
+	Point B(7, 8);
+	A.print();
+	B.print();
+
+	Point C = A + B;
+	C.print();
+
+	C = A - B;
+	C.print();
+
+	C = A * B;
+	C.print();
+
+	C = A / B;
+	C.print();
+
+	C++;
+	C.print();
+#endif // ARITHMETICAL_OPERATORS_CHECK
+
+	//cout << (2 == 3) << endl;
+	cout << (Point(2, 3) == Point(2, 3)) << endl;
 }
 
 /*
